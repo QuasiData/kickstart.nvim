1d2ea4209b6245828cf8aa8cf057b01d43264223
QuasiData <oscar.rotander@gmail.com>
2025-07-27 18:51:35 +0200

    Synced fork

--- init.lua
+++ init.lua
@@ -968,7 +968,7 @@ require('lazy').setup({
     priority = 1000,
     -- configuration is optional!
     opts = {
-      -- your settings here
+      --kyour settings here
     },
   },
   {
@@ -1071,7 +1071,7 @@ require('lazy').setup({
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
   --
   -- require 'kickstart.plugins.debug',
-  --require 'kickstart.plugins.indent_line',
+  require 'kickstart.plugins.indent_line',
   require 'kickstart.plugins.lint',
   require 'kickstart.plugins.autopairs',
   require 'kickstart.plugins.neo-tree',
